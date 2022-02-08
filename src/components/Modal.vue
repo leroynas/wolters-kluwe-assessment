fd4d71a6cbe3c53b15f397b4c74e7f73c62d78b1
= <=>
2022-02-08 14:28:28 +0100

    fix: show modal

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,6 +1,11 @@
 <template>
   <teleport to="body">
-    <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+    <div
+      class="modal fade show"
+      :style="{ display: 'block' }"
+      tabindex="-1"
+      role="dialog"
+    >
       <div class="modal-dialog" role="document">
         <div class="modal-content">
           <div class="modal-header">
