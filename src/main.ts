5e3008aa617afcccbbd25e2711f10dea03824939
= <=>
2022-02-08 12:50:47 +0100

    chore: setup project main

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,20 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import { createI18n } from 'vue-i18n';
 
-createApp(App).mount("#app");
+import App from './App.vue';
+import Button from './components/Button.vue';
+import Modal from './components/Modal.vue';
+import nl from './locales/nl.json';
+
+const app = createApp(App);
+const i18n = createI18n({
+  locale: 'nl',
+  messages: { nl },
+});
+
+app.use(i18n);
+
+app.component('app-button', Button);
+app.component('app-modal', Modal);
+
+app.mount('#app');
