12cad7cb5a0b9019c1687fe29bbfba86312a0cbc
= <=>
2022-02-08 12:50:34 +0100

    chore: update dependencies and project config

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,63 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <div class="container">
+    <div class="row">
+      <Overview :items="items" @addItem="addItem" @updateItem="updateItem" />
+
+      <Statistics :items="items" />
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+import { defineComponent, ref } from 'vue';
+import { v4 as uuid } from 'uuid';
+
+enum STATUSES {
+  OPEN = 'open',
+  CLOSED = 'closed',
+}
+
+type Item = {
+  id: string;
+  name: string;
+  subject: string;
+  status: STATUSES;
+};
+
+const INIT_ITEMS: Item[] = [
+  {
+    id: uuid(),
+    name: 'Jan Willem',
+    subject: 'Hoe kan ik inloggen?',
+    status: STATUSES.OPEN,
+  },
+  {
+    id: uuid(),
+    name: 'Jan Willem',
+    subject: 'Hoe kan ik inloggen?',
+    status: STATUSES.CLOSED,
+  },
+];
 
 export default defineComponent({
-  name: "App",
-  components: {
-    HelloWorld,
+  setup() {
+    const items = ref(INIT_ITEMS);
+
+    const addItem = (item: Item) => {
+      items.value = [item, ...items.value];
+    };
+
+    const updateItem = (item: Item) => {
+      items.value = items.value.map((stateItem) =>
+        stateItem.id === item.id ? item : stateItem,
+      );
+    };
+
+    return {
+      items,
+      addItem,
+      updateItem,
+    };
   },
 });
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
