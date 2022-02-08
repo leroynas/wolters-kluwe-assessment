2e0e04a0b579d189d0783c9598720c68bf108689
= <=>
2022-02-08 12:51:04 +0100

    feat: modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,34 @@
+<template>
+  <teleport to="body">
+    <div class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title">{{ title }}</h5>
+          </div>
+
+          <div class="modal-body">
+            <slot />
+          </div>
+
+          <div class="modal-footer" v-if="$slots.actions">
+            <slot name="actions" />
+          </div>
+        </div>
+      </div>
+    </div>
+  </teleport>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
