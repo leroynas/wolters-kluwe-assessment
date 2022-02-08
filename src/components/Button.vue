9e8693c663510d96ab4936694b2229d885f42435
= <=>
2022-02-08 12:50:58 +0100

    feat: button component

---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+  <button class="btn btn-sm" :class="`btn-${type}`" @click="$emit('click')">
+    <slot />
+  </button>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+enum TYPES {
+  PRIMARY = 'primary',
+  INFO = 'info',
+}
+
+export default defineComponent({
+  props: {
+    type: {
+      type: String as () => TYPES,
+      required: true,
+      validator: (value: TYPES) => Object.values(TYPES).includes(value),
+    },
+  },
+});
+</script>
